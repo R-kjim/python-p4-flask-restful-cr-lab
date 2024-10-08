8e59d55f64201ff1e13dd86fd27973788e419a71
R-kjim <rkayjim@gmail.com>
2024-10-08 14:50:10 +0300

    flask restful create

--- server/app.py
+++ server/app.py
@@ -17,11 +17,26 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants=Plant.query.all()
+        return make_response([plant.to_dict() for plant in plants],200)
+    
+    def post(self):
+        data=request.get_json()
+        new_plant=Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(new_plant.to_dict(),201)
 
 class PlantByID(Resource):
-    pass
+    def get(self,id):
+        return make_response(Plant.query.filter_by(id=id).first().to_dict(),200)
         
-
+api.add_resource(Plants,'/plants')
+api.add_resource(PlantByID,'/plants/<int:id>')
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
